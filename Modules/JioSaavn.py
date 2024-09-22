a4756189e722f720d3ade7f02323176b220c5ae2
Henry-Richard7 <henry.richard.2k@gmail.com>
2024-09-22 15:59:41 +0530

    Added JioSaavn Album Details Endpoint

--- Modules/JioSaavn.py
+++ Modules/JioSaavn.py
@@ -1,42 +1,60 @@
 from httpx import AsyncClient
 import base64
-from pyDes import des, ECB,PAD_PKCS5
-from models.JioSaavn import HomeModels
+from pyDes import des, ECB, PAD_PKCS5
+from models.JioSaavn import HomeModels, AlbumDetailsModel
+
 
 class JioSaavnApi:
     def __init__(self) -> None:
-        self.des_cipher = des(b"38346591", ECB, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5)
+        self.des_cipher = des(
+            b"38346591", ECB, b"\0\0\0\0\0\0\0\0", pad=None, padmode=PAD_PKCS5
+        )
         self.jio_api_base_url = "https://www.jiosaavn.com/api.php"
-        
-    async def decrypt_url(self, url:str)->str:
+
+    async def decrypt_url(self, url: str) -> str:
         enc_url = base64.b64decode(url.strip())
 
         dec_url = self.des_cipher.decrypt(enc_url, padmode=PAD_PKCS5).decode("utf-8")
         dec_url = dec_url.replace("_96.mp4", "_320.mp3")
         return dec_url
-    
-    async def home_page(self,language:str) -> HomeModels.HomePageResponse:
-        cookies = {
-            "L":language
-        }
-        
+
+    async def home_page(self, language: str) -> HomeModels.HomePageResponse:
+        cookies = {"L": language}
+
+        request_params = {"__call": "content.getHomepageData"}
+        async with AsyncClient() as async_client:
+            resp = await async_client.get(
+                self.jio_api_base_url, cookies=cookies, params=request_params
+            )
+
+        resp = resp.json()
+
+        new_albums = [
+            HomeModels.NewAlbumItem(**album) for album in resp.get("new_albums")
+        ]
+        featured_playlists = [
+            HomeModels.FeaturedPlaylistItem(**featured_playlist)
+            for featured_playlist in resp.get("featured_playlists")
+        ]
+        charts = [HomeModels.ChartItem(**chart) for chart in resp.get("charts")]
+
+        return HomeModels.HomePageResponse(
+            new_albums=new_albums, featured_playlists=featured_playlists, charts=charts
+        )
+
+    async def album_details(self, album_id: str) -> AlbumDetailsModel.AlbumDetails:
+
         request_params = {
-            "__call":"content.getHomepageData"
+            "__call": "content.getAlbumDetails",
+            "albumid": album_id,
         }
         async with AsyncClient() as async_client:
-            resp = await async_client.get(self.jio_api_base_url,cookies=cookies,params=request_params)
-            
+            resp = await async_client.get(self.jio_api_base_url, params=request_params)
+
         resp = resp.json()
-        
-        new_albums = [HomeModels.NewAlbumItem(**album) for album in resp.get("new_albums")]
-        featured_playlists = [HomeModels.FeaturedPlaylistItem(**featured_playlist) for featured_playlist in resp.get("featured_playlists")]
-        charts = [HomeModels.ChartItem(**chart) for chart in resp.get("charts")]
-        
-        return HomeModels.HomePageResponse(
-            new_albums=new_albums,
-            featured_playlists=featured_playlists,
-            charts=charts
-        )
-        
-    async def album_details(self, album_id:str):
-        pass
+
+        album_details = AlbumDetailsModel.AlbumDetail(**resp)
+        songs = [AlbumDetailsModel.Song(**song) for song in resp.get("songs")]
+
+        result = AlbumDetailsModel.AlbumDetails(album_detail=album_details, songs=songs)
+        return result
--- models/JioSaavn/AlbumDetailsModel.py
+++ models/JioSaavn/AlbumDetailsModel.py
@@ -1,28 +1,45 @@
 from pydantic import BaseModel, computed_field, Field, field_validator, ValidationInfo
 
-class Songs(BaseModel):
-    id:str
-    title:str = Field(...,alias='song')
-    album:str
-    year:str
-    artist:str = Field(...,alias='music')
-    artist_id:str = Field(...,alias='music_id')
-    image:str
 
-class AlbumDetails:
-    title: str
-    name : str
-    year : str
-    release_date : str
-    primary_artists : list
-    primary_artists_id : list
-    albumid : str
-    perma_url : str
-    image : str
-    
-    @field_validator('primary_artists','primary_artists_id',mode='before')
-    def string_to_list(cls, value:str,info:ValidationInfo):
+class Song(BaseModel):
+    id: str
+    title: str = Field(..., alias="song")
+    album: str
+    albumid: str
+    year: str
+    artist: str = Field(..., alias="music")
+    artist_id: str = Field(..., alias="music_id")
+    image: str
+    play_count: str
+    encrypted_media_url: str
+    duration: str
+    release_date: str
+
+    @field_validator("image", mode="before")
+    def image_resolution_fix(cls, value: str, info: ValidationInfo):
         if info.field_name == "image":
-            return value.replace("150x150",'500x500')
-        elif isinstance(value,str):
+            return value.replace("150x150", "500x500")
+
+
+class AlbumDetail(BaseModel):
+    title: str
+    name: str
+    year: str
+    release_date: str
+    primary_artists: list
+    primary_artists_id: list
+    albumid: str
+    perma_url: str
+    image: str
+
+    @field_validator("primary_artists", "primary_artists_id", "image", mode="before")
+    def string_to_list(cls, value: str, info: ValidationInfo):
+        if info.field_name == "image":
+            return value.replace("150x150", "500x500")
+        elif isinstance(value, str):
             return value.split(", ")
+
+
+class AlbumDetails(BaseModel):
+    album_detail: AlbumDetail
+    songs: list[Song]
